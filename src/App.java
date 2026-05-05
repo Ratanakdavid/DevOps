f0f1c0216a791d8256c1bad7011c923d44a382dc
Ratanakdavid <vidsmile18@gmail.com>
2026-05-06 05:36:25 +0700

    right 1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.printf("Hello, World %d!", 22;
+        System.out.printf("Hello, World %d!", 42);
     }
 }
